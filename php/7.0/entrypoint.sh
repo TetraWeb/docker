1908425f313f642e7230324a85649fa4ff3bb36f
Dmitry Lukashin <dlukashin@tetraweb.ru>
2015-07-19 18:33:43 +0300

    Try to keep common entrypoint.sh

--- php/7.0/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-if [[ "$WITH_XDEBUG" == 1 && -f /usr/local/etc/php/conf.d/xdebug.ini.disabled ]]
-then
-  mv /usr/local/etc/php/conf.d/xdebug.ini.disabled /usr/local/etc/php/conf.d/xdebug.ini
-fi
-
-exec "$@"
