1df032029db21aad74c6401552e68cb473399c08
Dmitry Lukashin <dlukashin@tetraweb.ru>
2015-07-19 19:21:28 +0300

    Provide composer github token via environment variable

--- php/5.5/entrypoint.sh
+++ php/5.5/entrypoint.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 set -e
 
+# With env variable WITH_XDEBUG=1 xdebug extension will be enabled
 if [[ "$WITH_XDEBUG" == 1 && -f /usr/local/etc/php/conf.d/xdebug.ini.disabled ]]
 then
   mv /usr/local/etc/php/conf.d/xdebug.ini.disabled /usr/local/etc/php/conf.d/xdebug.ini
+fi
+
+# Provide github token if you are using composer a lot in non-interactive mode
+# Otherwise one day it will get stuck with request for authorization
+# https://github.com/settings/tokens
+if [[ ! -z "$COMPOSER_GITHUB" ]]
+then
+  composer config --global github-oauth.github.com "$COMPOSER_GITHUB"
 fi
 
 exec "$@"
