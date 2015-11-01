51890b80fcaf3a4b4d5585a00b340eb38bd9f525
Dmitry Lukashin <dlukashin@gmail.com>
2015-11-01 13:00:04 +0300

    Move parsing to parameters

--- gitlab-runner-vm/bootstrap.sh
+++ gitlab-runner-vm/bootstrap.sh
@@ -101,9 +101,9 @@ do_install() {
         ENVVARS+=("TIMEZONE=$TIMEZONE")
     fi
 
-    gitlab-ci-multi-runner register -n -r "$CI_TOKEN" -u "$CI_URL" --tag-list 'php,mysql' --executor docker --docker-image tetraweb/php:latest $(printf " --env %s" "${ENVVARS[@]}")
-    echo "    allowed_images = [\"tetraweb/php:*\"]" >> /etc/gitlab-runner/config.toml
-    echo "    allowed_services = [\"*\", \"*/*\"]" >> /etc/gitlab-runner/config.toml
+    gitlab-ci-multi-runner register -n -r "$CI_TOKEN" -u "$CI_URL" --tag-list 'php,mysql' --executor docker \
+        --docker-image "tetraweb/php:latest" --docker-allowed-images "tetraweb/php:*" \
+        --docker-allowed-services "*" --docker-allowed-services "*/*" $(printf " --env \"%s\"" "${ENVVARS[@]}")
 
     sed -i -- "s/concurrent = 1/concurrent = $CONCURRENT/g" /etc/gitlab-runner/config.toml
 
