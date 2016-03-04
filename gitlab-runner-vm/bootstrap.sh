5da8ffd66ecf2aacce27ad8abcc596083b13e135
Dmitry Lukashin <dlukashin@gmail.com>
2016-03-04 03:50:07 +0300

    Disable scheduled updates of images since it works in gitlab runner

--- gitlab-runner-vm/bootstrap.sh
+++ gitlab-runner-vm/bootstrap.sh
@@ -110,10 +110,11 @@ do_install() {
     sed -i -- "s/concurrent = 1/concurrent = $CONCURRENT/g" /etc/gitlab-runner/config.toml
 
     cronjob="#!/bin/bash\n"
-    for phpver in 5.2 5.3 5.4 5.5 5.6 7.0
-    do
-        cronjob+="docker pull tetraweb/php:$phpver\n"
-    done
+#    Automatic updates of images seems to be working in gitlab-runner
+#    for phpver in 5.2 5.3 5.4 5.5 5.6 7.0
+#    do
+#        cronjob+="docker pull tetraweb/php:$phpver\n"
+#    done
 
     # Cleanup orphaned images
     cronjob+="docker rmi \$(docker images | grep none | awk '{print \$3}')\n"
